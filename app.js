75f0e88a7c0265219e82c2156f0d145f8498e397
GeovannyTS <yowtecum@gmail.com>
2023-10-27 11:42:35 -0600

    Validator & menu

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 require('colors')
 
+const {mostrarMenu, pause} = require('./helpers/mensajes.js')
+
 console.clear();
 
-const main = async =>{
+const main = async() =>{
 
-    console.log('Hola mundo');
+    console.log('Holaaa');
+
+    let option = '';
+
+    do {
+        option = await mostrarMenu();
+        if( option !== '0') await pause();       
+
+    } while ( option !== '0' );
+
+    
 
 
 } 
--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -1,6 +1,50 @@
+const { resolve } = require('path');
+
 require('colors')
 
 
 const mostrarMenu = () =>{
     
+    return new Promise( resolve => {
+        console.clear();
+        console.log('==========================='.green);
+        console.log('   Seleccione una Opcion '.green);
+        console.log('===========================\n'.green);
+    
+        console.log(`${'1.'.green} Crear tarea`);
+        console.log(`${'2.'.green} Lista de tareas`);
+        console.log(`${'3.'.green} Lista de tareas completadas`);
+        console.log(`${'4.'.green} Lista de tareas pendientes`);
+        console.log(`${'5.'.green} Completar tarea`);
+        console.log(`${'6.'.green} Eliminar tarea`);
+        console.log(`${'0.'.green} Salir \n`);
+    
+        // Recibir informacion 
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+    
+        readline.question('Seleccione una opcion: ', (answ) =>{
+            readline.close();
+            resolve(answ);
+        })
+    })
 }
+
+const pause = () => {
+
+    return new Promise(  resolve =>{
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+    
+        readline.question(`\nPresione ${ 'Enter'.green} para continuar\n`, (answ) =>{
+            readline.close();
+            resolve(answ)
+        })
+    } )
+}
+
+module.exports = {mostrarMenu, pause};
